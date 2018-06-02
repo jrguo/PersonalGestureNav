c6878d2b79016156b4abf750e549347573d96569
Jerry <jrguo2@illinois.edu>
2018-06-02 12:21:24 -0500

    Added some more settings.

--- feature/src/main/java/com/jrguo2/personalgesturenav/settings/NavAreaSettings.java
+++ feature/src/main/java/com/jrguo2/personalgesturenav/settings/NavAreaSettings.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import com.jrguo2.personalgesturenav.feature.R;
 import com.jrguo2.personalgesturenav.settings.utils.FloatSeekBarListener;
 import com.jrguo2.personalgesturenav.settings.utils.IntSeekerListener;
+import com.jrguo2.personalgesturenav.settings.utils.LongSeekBarListener;
 import com.jrguo2.personalgesturenav.utils.Configs;
 
 public class NavAreaSettings extends Fragment {
@@ -59,11 +60,62 @@ public class NavAreaSettings extends Fragment {
         textView = view.findViewById(R.id.navBarHeightText);
         seekBar= view.findViewById(R.id.navBarHeightSeekBar);
         min = 0;
-        max = 300;
-        progress = (int)(((double)Configs.getInt("navBarHeight", 100) * 100) / (max - min));
+        max = 100;
+        progress = (int)(((double)Configs.getFloat("navBarHeight", 100) * 100) / (max - min));
         seekBar.setProgress(progress);
-        textView.setText("Nav Bar Height:\t" + Configs.getInt("navBarHeight", 100));
+        textView.setText("Nav Bar Height:\t" + Configs.getFloat("navBarHeight", 100));
         seekBar.setOnSeekBarChangeListener(new FloatSeekBarListener("Nav Bar Height", textView, "navBarHeight", min, max));
+
+        //Navbar radius
+        textView = view.findViewById(R.id.navBarRadiusText);
+        seekBar= view.findViewById(R.id.navBarRadiusSeekBar);
+        min = 0;
+        max = 100;
+        progress = (int)(((double)Configs.getFloat("navBarRadius", 100) * 100) / (max - min));
+        seekBar.setProgress(progress);
+        textView.setText("Nav Bar Height:\t" + Configs.getFloat("navBarHeight", 100));
+        seekBar.setOnSeekBarChangeListener(new FloatSeekBarListener("Nav Bar Radius", textView, "navBarRadius", min, max));
+
+        //Nav-area x-offset
+        textView = view.findViewById(R.id.navAreaXOffsetText);
+        seekBar= view.findViewById(R.id.navAreaXOffsetSeekbar);
+        min = 0;
+        max = 50;
+        progress = (int)(((double)Configs.getInt("navAreaXOffset", 100) * 100) / (max - min));
+        seekBar.setProgress(progress);
+        textView.setText("Nav Area X-Offset:\t" + Configs.getInt("navAreaXOffset", 100));
+        seekBar.setOnSeekBarChangeListener(new IntSeekerListener("Nav Area X-Offset", textView, "navAreaXOffset", min, max));
+
+        //Nav-area y-offset
+        textView = view.findViewById(R.id.navAreaYOffsetText);
+        seekBar= view.findViewById(R.id.navAreaYOffsetSeekbar);
+        min = 0;
+        max = 50;
+        progress = (int)(((double)Configs.getInt("navAreaYOffset", 100) * 100) / (max - min));
+        seekBar.setProgress(progress);
+        textView.setText("Nav Area X-Offset:\t" + Configs.getInt("navAreaYOffset", 100));
+        seekBar.setOnSeekBarChangeListener(new IntSeekerListener("Nav Area Y-Offset", textView, "navAreaYOffset", min, max));
+
+        //Vibration intensity
+        textView = view.findViewById(R.id.vibrateIntensityText);
+        seekBar= view.findViewById(R.id.vibrateIntensitySeekbar);
+        min = 0;
+        max = 100;
+        progress = (int)(((double)Configs.getInt("vib_amplitude", 5) * 100) / (max - min));
+        seekBar.setProgress(progress);
+        textView.setText("Vibration Intensity:\t" + Configs.getInt("vib_amplitude", 100));
+        seekBar.setOnSeekBarChangeListener(new IntSeekerListener("Vibration Intensity", textView, "vib_amplitude", min, max));
+
+        //Vibration length
+        textView = view.findViewById(R.id.vibrateLengthText);
+        seekBar= view.findViewById(R.id.vibrateLengthSeekbar);
+        min = 0;
+        max = 500;
+        progress = (int)(((double)Configs.getInt("vib_duration", 5) * 100) / (max - min));
+        seekBar.setProgress(progress);
+        textView.setText("Vibration Length:\t" + Configs.getInt("vib_duration", 100));
+        seekBar.setOnSeekBarChangeListener(new LongSeekBarListener("Vibration Length", textView, "vib_duration", min, max));
+
     }
 
 
